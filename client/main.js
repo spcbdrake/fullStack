ee32723adaaa0fc057aa9491f6ac308a9f0ebbdb
tken85 <terrykennair@Terrys-MBP.attlocal.net>
2015-11-08 21:06:00 -0500

    working site

--- client/main.js
+++ client/main.js
@@ -55,8 +55,43 @@ var page = {
       });
 
     });
+    $('.submitPicks').on('click', function(event){
+      event.preventDefault();
+      if(page.timesSubmitted < 1){
+        page.runMatches();
+        page.howMuchWon();
+        page.timesSubmitted++;
+      }
+      else{
+      alert("You have already played today");
+      }
+    });
+    $('.nextDay').on('click', function(event){
+      event.preventDefault();
+      page.currentMatches = [];
+      page.currentPicks = [{name: "none"},{name: "none"},{name: "none"}];
+      page.currentWinners = [];
+      page.match1 = [];
+      page.match2 = [];
+      page.match3 = [];
+      page.timesSubmitted = 0;
+      $('#dailyWinners').html("");
+      $('#dailyWinnings').html("");
+      $('#yourPicks').html("");
+      page.grabMatches();
+    });
     $('.logoutBtn').on('click', function(event){
       event.preventDefault();
+      page.currentMatches = [];
+      page.currentPicks = [{name: "none"},{name: "none"},{name: "none"}];
+      page.currentWinners = [];
+      page.match1 = [];
+      page.match2 = [];
+      page.match3 = [];
+      page.timesSubmitted = 0;
+      $('#dailyWinners').html("");
+      $('#dailyWinnings').html("");
+      $('#yourPicks').html("");
       $.ajax({
         method: 'Post',
         url: '/logout',
@@ -76,6 +111,13 @@ var page = {
     $('.colRight').on('click', 'article', function(event){
       event.preventDefault();
       var whichClicked = $(this).data('index');
+      var $imgChild = $(this).children('img');
+      if($imgChild.hasClass('selected')){
+        console.log("selected already");
+        $imgChild.removeClass('selected');
+      }
+      else{
+      $imgChild.addClass('selected');
       if(whichClicked < 2){
         page.currentPicks[0]= page.currentMatches[whichClicked];
       }
@@ -85,22 +127,9 @@ var page = {
       else{
         page.currentPicks[2] = page.currentMatches[whichClicked];
       }
+      $('#yourPicks').html("Your Picks: "+ page.currentPicks[0].name + ", "+ page.currentPicks[1].name + ", "+ page.currentPicks[2].name);
+    }
     });
-  },
-   //this is just a quick workaround I was using to instantly create users in the console. Not used in the end product
-  addNewUser: function(userInfo){
-    $.ajax({
-      method: 'POST',
-      url: page.url,
-      data: userInfo,
-      success: function(){
-        console.log("Well that worked");
-      },
-      failure: function(){
-        console.log("What's going on?");
-      }
-    });
-
   },
   grabMatches: function(){
     $.ajax({
@@ -160,27 +189,22 @@ var page = {
     page.currentPicks = [pick1.name, pick2.name, pick3.name];
   },
   runMatches: function(){
-    /*var match1 = page.currentMatches.slice(0,2);
-      console.log("Match 1", match1);
-      var chance1 = match1[0].level/(match1[0].level+match1[1].level);
-      var chance2 = 1-chance1;
-      var payOut1 = match1[1].level/match1[0].level;
-      var payOut2 = 1/payOut1;
-      match1[0].odds = chance1;
-      match1[0].payOut = payOut1;
-      match1[1].odds = chance2;
-      match1[1].payOut = payOut2;
-      console.log("Odds 1: " + chance1 + " Odds 2: " + (1-chance1) );*/
-      page.currentWinners[0]=page.whoWins(match1[0], match1[1]);
-    //var match2 = page.currentMatches.slice(2, 4);
-      console.log("Match 2", match2);
-      page.currentWinners[1]=page.whoWins(match2[0], match2[1]);
-  //  var match3 = page.currentMatches.slice(4,6);
-      console.log("Match 3", match3);
-      page.currentWinners[2]=page.whoWins(match3[0], match3[1]);
+      page.currentWinners[0]=page.whoWins(page.match1);
+      page.currentWinners[1]=page.whoWins(page.match2);
+      page.currentWinners[2]=page.whoWins(page.match3);
   },
-  whoWins: function(player1, player2){
-    var chance1 = player1.level/(player1.level + player2.level);
+  whoWins: function(match){
+    var randomRunner = Math.random();
+    console.log(randomRunner);
+    if(randomRunner <= match[0].odds){
+      console.log(match[0].name + "Wins");
+      return match[0].name;
+    }
+    else{
+      console.log(match[1].name + "Wins");
+      return match[1].name;
+    }
+    /*var chance1 = player1.level/(player1.level + player2.level);
     var randomRunner = Math.random();
     console.log(randomRunner);
     if(randomRunner <= chance1){
@@ -190,22 +214,34 @@ var page = {
     else{
       console.log(player2.name + "Wins");
       return player2.name;
-    }
+    }*/
   },
   howMuchWon: function(){
     var wins=0;
+    var dailyPayout = 0;
     for(var i=0; i<page.currentWinners.length; i++){
-      if(page.currentWinners[i] === page.currentPicks[i]){
+      if(page.currentWinners[i] === page.currentPicks[i].name){
         console.log("you picked correctly and won! good job, champ");
         wins++;
-        page.currentUser.money += 20;
+        page.currentUser.money += Math.round(20*page.currentPicks[i].payOut);
+        dailyPayout += Math.round(20*page.currentPicks[i].payOut);
+        console.log("won" + dailyPayout);
 
       }
       else{
+        if(page.currentPicks[i].name ==="none"){
+          console.log("you refrained from picking");
+        }
+        else{
         console.log("you picked incorrectly and lost");
         page.currentUser.money -= 20;
+        dailyPayout -=20;
+        console.log("lost" + dailyPayout);
+      }
       }
     }
+    $('#dailyWinners').html("Daily Winners: " + page.currentWinners[0] + ", "+page.currentWinners[1] + ", " + page.currentWinners[2]);
+    $('#dailyWinnings').html("Your Daily Winnings: $"+ dailyPayout);
     $('#userMoney').html(page.currentUser.money);
     var money = {money: page.currentUser.money};
     console.log(money);
@@ -217,6 +253,7 @@ var page = {
         console.log("Received that shit");
         console.log(resp);
         console.log(money);
+        page.pullTopTen();
       },
       failure: function(){
         console.log("What the fuck");
@@ -243,11 +280,12 @@ var page = {
     $('#theBest').html(topHtml);
   },
   currentMatches : [],
-  currentPicks: [{},{},{}],
+  currentPicks: [{name: "none"},{name: "none"},{name: "none"}],
   currentWinners: [],
   currentUser: {},
   match1: [],
   match2: [],
-  match3: []
+  match3: [],
+  timesSubmitted: 0,
 
 };
